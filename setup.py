dcd774b1d33389b4eabf3b7cbf1f97f7e623ef48
Corey Spohn <cas584@cornell.edu>
2022-09-21 11:36:27 -0400

    Formatting setup

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-'''
+"""
 Sets up exoVista_wrapper, a package to make it easier to interface with
 exoVista data using python
-'''
+"""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 setup(
-    name='exoVista_wrapper',
-    version='0.00001',
-    packages=find_packages(include=['exoVista_wrapper', 'exoVista_wrapper.*']),
+    name="exoVista_wrapper",
+    version="0.1",
+    packages=find_packages(include=["exoVista_wrapper", "exoVista_wrapper.*"]),
     install_requires=[
-        'astropy',
-        'matplotlib',
-        'numpy',
-        'pandas',
-        'tqdm',
-    ]
+        "astropy",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "tqdm",
+    ],
 )
